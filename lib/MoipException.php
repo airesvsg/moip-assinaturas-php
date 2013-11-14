35f9164d35c20ffe12e2786e093b3de72da939aa
Aires Gonçalves <airesvsg@gmail.com>
2013-11-14 10:31:47 -0200

    Update MoipException.php

--- lib/MoipException.php
+++ lib/MoipException.php
@@ -6,8 +6,8 @@
      	
 	class MoipException extends Exception {
 
-		public function __construct( $message = null, $code = 0, Exception $previous = null ) {
-	        parent::__construct( $message, $code, $previous );
+		public function __construct( $message = null, $code = 0 ) {
+	        parent::__construct( $message, $code );
 	    }
 
 	}
